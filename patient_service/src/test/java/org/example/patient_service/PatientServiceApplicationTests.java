1d8a7baa171f7e2d57443ba5808bf22e193b0d46
vidhanshu37 <kelawalavidhanshu@gmail.com>
2026-04-28 15:09:26 +0530

    Initial commit

---
+++ patient_service/src/test/java/org/example/patient_service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
