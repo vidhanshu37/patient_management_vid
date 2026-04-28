cf29dcf512a9b59405b6728fe106c1b60b55fd35
vidhanshu37 <kelawalavidhanshu@gmail.com>
2026-04-28 15:12:12 +0530

    billing-service

---
+++ billing_service/src/test/java/org/example/billing_service/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
