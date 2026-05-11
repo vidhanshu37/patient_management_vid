0990e0c91923a5600ea779579e0b47d2cf29da29
vidhanshu37 <kelawalavidhanshu@gmail.com>
2026-05-11 21:52:40 +0530

    dockerize auth-service

--- auth_service/src/main/resources/data.sql
+++ auth_service/src/main/resources/data.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS "users" (
 );
 
 -- Insert the user if no existing user with the same id or email exists
-INSERT INTO "users" (email, password, role)
-VALUES ( 'testuser@test.com', '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN')
+INSERT INTO "users" (id, email, password, role)
+VALUES ( '223e4567-e89b-12d3-a456-426614174006', 'testuser@test.com', '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN')
 ON CONFLICT (email) DO NOTHING;
