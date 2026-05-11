248f35b5e6ba646d494fcf241effcacda23c7968
vidhanshu37 <kelawalavidhanshu@gmail.com>
2026-05-11 20:40:05 +0530

    user model

---
+++ auth_service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- Ensure the 'users' table exists
+CREATE TABLE IF NOT EXISTS "users" (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+-- Insert the user if no existing user with the same id or email exists
+INSERT INTO "users" (email, password, role)
+VALUES ( 'testuser@test.com', '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN')
+ON CONFLICT (email) DO NOTHING;
