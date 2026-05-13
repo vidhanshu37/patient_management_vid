5c5573aeed3d5882488852798b9c72f7aa9cf5b7
vidhanshu37 <kelawalavidhanshu@gmail.com>
2026-05-13 16:37:53 +0530

    add auth-service interception in API Gateway

---
+++ api_gateway/src/main/java/org/example/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,52 @@
+package org.example.api_gateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationGatewayFilterFactory extends  AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient webClient;
+
+    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder, @Value("${auth.service.url}") String authServiceUrl) {
+        this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if(token == null || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+//            return webClient.get()
+//                    .uri("/validate")
+//                    .header(HttpHeaders.AUTHORIZATION, token)
+//                    .retrieve()
+//                    .bodyToMono(Boolean.class)
+//                    .flatMap(isValid -> {
+//                        if (isValid) {
+//                            return chain.filter(exchange);
+//                        } else {
+//                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//                            return exchange.getResponse().setComplete();
+//                        }
+//                    });
+
+            return webClient.get()
+                    .uri("/validate")
+                    .header(HttpHeaders.AUTHORIZATION, token)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .then(chain.filter(exchange));
+        };
+    }
+}
